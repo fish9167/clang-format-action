69a6fa7806a891384895eec01351210d5686af47
fish9167 <78347405+fish9167@users.noreply.github.com>
2021-03-24 20:50:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ SRC=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(c\|h\|hpp\|cpp\|cxx\)\$" | 
 
 # Run clang-format over all the matching files
 echo "Using style $1"
-clang-format -style=$1 -i $SRC
+clang-format -style=$1 -i $SRC >> clang-format-report.txt
 
 # Check to see if there is anything to be done
 # If so commit and push. Otherwise do nothing
