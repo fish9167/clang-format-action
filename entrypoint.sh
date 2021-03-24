5e8f44ffa36c4ed9cacff3949c4dbdd2fe21e473
fish9167 <78347405+fish9167@users.noreply.github.com>
2021-03-24 17:58:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,18 +15,18 @@ clang-format -style=$1 -i $SRC
 # If so commit and push. Otherwise do nothing
 if ! git diff --quiet; then
   # Configure the author
-  echo ">> Configuring the author"
-  git config --global user.email "clang-format@github-actions"
-  git config --global user.name "clang-format"
+  #echo ">> Configuring the author"
+  #git config --global user.email "clang-format@github-actions"
+  #git config --global user.name "clang-format"
 
   # Commit the changes
-  echo ">> Committing the changes"
-  git commit -a -m "Apply clang-format" || true
+  #echo ">> Committing the changes"
+  #git commit -a -m "Apply clang-format" || true
 
   # Push to the branch
-  BRANCH=${GITHUB_REF#*refs/heads/}
-  echo ">> Pushing to $BRANCH"
-  git push -u origin $BRANCH
+  #BRANCH=${GITHUB_REF#*refs/heads/}
+  #echo ">> Pushing to $BRANCH"
+  #git push -u origin $BRANCH
 
   # Set a message about what happened
   #MSG="Changes are applied, committed, and pushed!"
