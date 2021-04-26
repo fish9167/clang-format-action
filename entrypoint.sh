12ad7dd50df8f523ad5aeefc3f7b0211615df7c4
fish9167 <674821954@qq.com>
2021-04-26 13:47:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,32 +22,8 @@ sed -i 's/modified://g' change.list
 cat change.list
  
 if ! git diff --quiet; then
-  # Configure the author
-  echo ">> Configuring the author"
-  git config --global user.email "clang-format@github-actions"
-  git config --global user.name "clang-format"
-
-  # Commit the changes
-  #echo "get last_commit_msg"
-  #last_commit_msg=`git show --format=%s | head -n 1`
-  #echo ${last_commit_msg}
-  echo ">> Committing the changes"
-  git commit -a -m "Auto apply clang-format (Github Action)" || true
-
-  # Push to the branch
-  BRANCH=${GITHUB_REF#*refs/heads/}
-  echo ">> Pushing to $BRANCH"
-  echo "push $BRANCH"
-  git push -u origin $BRANCH
-
-  # Set a message about what happened
   MSG="Changes are applied, committed, and pushed!"
-  #MSG=`git diff`
-  echo "::set-output name=message::$MSG"
-  #exit 1
 else
   MSG="There are no changes, all good!"
 fi
-
-# Push the message
 echo "::set-output name=message::$MSG"
