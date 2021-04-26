0bccd3d245055c5ae5a0158397bb16bb61b61e77
fish9167 <674821954@qq.com>
2021-04-26 14:45:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,16 +14,17 @@ echo "Using style $1"
 clang-format -style=$1 -i $SRC
 # Check to see if there is anything to be done
 # If so commit and push. Otherwise do nothing
-echo "--------------------------------"
-echo "Illegal files:"
-echo "--------------------------------"
-git status | grep modified >> change.list
-sed -i 's/modified://g' change.list
-cat change.list
-rm -f change.list
+
  
 if ! git diff --quiet; then
   MSG="Changes are applied, committed, and pushed!"
+  echo "--------------------------------"
+  echo "Illegal files:"
+  echo "--------------------------------"
+  git status | grep modified >> change.list
+  sed -i 's/modified://g' change.list
+  cat change.list
+  rm -f change.list
 else
   MSG="There are no changes, all good!"
 fi
